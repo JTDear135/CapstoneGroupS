94e7526f61d478d0175b47e4fd2f963290c51a05
Jacob Dearborn <jtdear4@ou.edu>
2025-02-21 12:53:14 -0600

    [CAP-16] Added Simple Unit Test

--- flight_code_validator/flight_code_validator.py
+++ flight_code_validator/flight_code_validator.py
@@ -42,7 +42,6 @@ class FlightCodeValidator:
             airport_details = df.loc[df[5] == airport_code]
             # Checks from United States
             if "United States" in airport_details[3].values:
-                print("I Am From The United States")
                 if "Pacific/Honolulu" in airport_details[11].values:
                     # Checks from Hawaii
                     return False
@@ -57,3 +56,38 @@ class FlightCodeValidator:
         # Airport does not exist
         else:
             return False
+        
+import unittest
+from unittest.mock import patch
+
+class TestFlightCodeValidator(unittest.TestCase):
+    
+    @patch("pandas.read_csv")
+    def test_airport_validation(self, mock_read_csv):
+        # Mock airport data
+        mock_read_csv.return_value = pd.DataFrame({
+            3: ["United States", "United States", "United States", "Canada"],
+            4: ["JFK", "ANC", "HNL", "YYZ"],  # IATA codes
+            5: ["KJFK", "PANC", "PHNL", "CYYZ"],  # ICAO codes
+            11: ["America/New_York", "America/Anchorage", "Pacific/Honolulu", "America/Toronto"]
+        })
+
+        # Valid US airport
+        self.assertTrue(FlightCodeValidator.is_valid("JFK"))
+        self.assertTrue(FlightCodeValidator.is_valid("KJFK"))
+
+        # Alaska & Hawaii airports (should be invalid)
+        self.assertFalse(FlightCodeValidator.is_valid("ANC"))
+        self.assertFalse(FlightCodeValidator.is_valid("PANC"))
+        self.assertFalse(FlightCodeValidator.is_valid("HNL"))
+        self.assertFalse(FlightCodeValidator.is_valid("PHNL"))
+
+        # Non-US airport
+        self.assertFalse(FlightCodeValidator.is_valid("YYZ"))
+        self.assertFalse(FlightCodeValidator.is_valid("CYYZ"))
+
+        # Non-existent airport
+        self.assertFalse(FlightCodeValidator.is_valid("XYZ"))
+
+if __name__ == "__main__":
+    unittest.main()
